c82f75c9e2900b20ebf67ab0b03c199cba041193
a.warzecha <a.warzecha89@gmail.com>
2018-10-28 19:27:39 +0100

    add word test changed

--- src/test/java/AddWordTest.java
+++ src/test/java/AddWordTest.java
@@ -1,11 +1,13 @@
 import app.Word;
-import app.WordRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
@@ -23,17 +25,26 @@ public class AddWordTest {
     public void setUp() {
         template = new RestTemplate();
         template.delete("http://localhost:8080/words/delete");
+        word = new Word();
+        word.setWord("jeden");
+        word.setTranslation("uno");
     }
 
     @Test
     public void addWord() {
-        word = new Word();
-        word.setWord("jeden");
-        word.setTranslation("uno");
+
         ResponseEntity<Word> wordResponseEntity = template.postForEntity("http://localhost:8080/words", word, Word.class);
         Word addedWord = wordResponseEntity.getBody();
         assertEquals(word.getWord(), addedWord.getWord());
         assertEquals(word.getTranslation(), addedWord.getTranslation());
+    }
+
+    @Test
+    public void addWord2() {
+        Map<String, String> map = new HashMap<>();
+        map.put("String", "String");
+        ResponseEntity<Word> wordResponseEntity = template.postForEntity("http://localhost:8080/words", word, Word.class, map);
+        wordResponseEntity.getStatusCode();
     }
 
 }
